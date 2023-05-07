b9e7c3a5c582a446ec3f8734c96dc0935db2ff57
wanlinainai <zxh15166423670@gmail.com>
2023-05-07 09:53:30 +0800

    Second commit

---
+++ service-oa/src/main/java/com/atguigu/auth/mapper/SysUserRoleMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.auth.mapper;
+
+import com.atguigu.model.system.SysUserRole;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户角色 Mapper 接口
+ * </p>
+ *
+ * @author atguigu
+ * @since 2023-05-06
+ */
+public interface SysUserRoleMapper extends BaseMapper<SysUserRole> {
+
+}
